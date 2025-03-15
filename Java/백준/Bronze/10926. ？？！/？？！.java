d0a2712236809752add8c38d9986b021d8c0cd50
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-03-15 23:50:30 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String  a = scan.next();
+
+
+        System.out.println(a + "??!");
+    }
+}
