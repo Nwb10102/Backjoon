c067f726007125b819a1a7d44f9a99cf074a01a8
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-03-28 16:12:57 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    String str1 = "         ,r'\"7";
+    System.out.println(str1);
+    System.out.println("r`-_   ,'  ,/");
+    System.out.println(" \\. \". L_r'");
+    System.out.println("   `~\\/");
+    System.out.println("      |");
+    System.out.println("      |");
+  }
+}
