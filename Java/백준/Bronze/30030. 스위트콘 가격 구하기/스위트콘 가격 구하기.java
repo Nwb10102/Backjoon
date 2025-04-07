11e1e13def3f2f9d5497c7c7a1855e6ddc0d434c
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-04-07 21:47:56 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ Java/백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int price = sc.nextInt();
+
+    System.out.println(price * 100 / 110);
+  }
+}
