cc14c53d769e7c0312aa82ae70d006ccff0f8313
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-04-14 23:30:35 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 172 ms, Memory: 17424 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner (System.in);
+    while(true){
+      String str = sc.next();
+      int age = sc.nextInt();
+      int kg = sc.nextInt();
+
+      if(str.equals("#") || age == 0 || kg == 0){
+        break;
+      }
+      
+      if(age > 17 || kg >= 80){
+        System.out.println(str + " Senior");
+      }else System.out.println(str + " Junior");
+    }
+  }
+}
