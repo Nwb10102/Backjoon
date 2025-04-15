9890ba4539bbec69979242c4569f6219c80473dc
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-04-15 17:44:20 +0900

    [Bronze IV] Title: 평균 점수, Time: 180 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int sum = 0;
+    for(int i = 1; i <= 5; i++){
+      int a = sc.nextInt();
+      if(a < 40){
+        sum += 40;
+      }else sum += a;
+    }
+    System.out.println((int)sum/5);
+  }
+}
