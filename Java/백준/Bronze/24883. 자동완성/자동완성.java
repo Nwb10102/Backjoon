b51549941c35afbce385914e4ebead380d69af37
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-04-19 21:51:32 +0900

    [Bronze V] Title: 자동완성, Time: 176 ms, Memory: 17688 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+
+public class Main
+{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+
+    if(str.equals("N") || str.equals("n")){
+      System.out.println("Naver D2");
+    }else System.out.println("Naver Whale");
+  }
+}
