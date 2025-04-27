d091dbce40128302cc818416231393ca4d900bcb
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-04-27 22:47:39 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

---
+++ Java/백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.java
@@ -0,0 +1,45 @@
+import java.math.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    switch(n){
+        case 1:
+            System.out.printf("12 1600");
+            break;
+        case 2:
+            System.out.printf("11 894");
+            break;
+        case 3:
+            System.out.printf("11 1327");
+            break;
+        case 4:
+            System.out.printf("10 1311");
+            break;
+        case 5:
+            System.out.printf("9 1004");
+            break;
+        case 6:
+            System.out.printf("9 1178");
+            break;
+        case 7:
+            System.out.printf("9 1357");
+            break;
+        case 8:
+            System.out.printf("8 837");
+            break;
+        case 9:
+            System.out.printf("7 1055");
+            break;
+        case 10:
+            System.out.printf("6 556");
+            break;
+        case 11:
+            System.out.printf("6 773");
+            break;
+    }
+  }
+}
