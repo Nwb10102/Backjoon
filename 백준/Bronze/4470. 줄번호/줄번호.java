a4af9e5e56bef48fddbf61c57dd8fb2e1996cc07
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-04-30 11:40:13 +0900

    [Bronze IV] Title: 줄번호, Time: 188 ms, Memory: 18440 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      sc.nextLine();
+
+      for(int i = 1; i <= n; i++) {
+        String str = sc.nextLine();
+        System.out.println(i + ". " + str);
+      }
+    }
+}
