cab62af97c1f056259cb61fd072eedd29edde437
jihooooooon <150505825+Nwb10102@users.noreply.github.com>
2025-06-10 00:02:55 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ Java/백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,21 @@
+import java.math.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int soongsil = sc.nextInt();
+		int korea = sc.nextInt();
+		int hanyang = sc.nextInt();
+
+		if(soongsil + korea + hanyang >= 100){
+			System.out.println("OK");
+		}else if(soongsil <= korea && soongsil <= hanyang){
+			System.out.println("Soongsil");
+		}else if(korea <= soongsil && korea <= hanyang){
+			System.out.println("Korea");
+		}else if(hanyang <= soongsil && hanyang <= korea){
+			System.out.println("Hanyang");
+		}
+	}
+}
